5c7e4d85fb3f88a22cdc2267836fbcc3b6a25413
Raphaël M <Galixte@users.noreply.github.com>
2020-12-08 16:41:20 +0100

    Fixs “rps_version” unexpectedly does not exist error during update.

--- migrations/release_1_4_2.php
+++ migrations/release_1_4_2.php
@@ -14,11 +14,11 @@ namespace paybas\quickstyle\migrations;
 use phpbb\db\migration\migration;
 
 /**
- * Class release_1_4_0
+ * Class release_1_4_2
  *
  * @package paybas\quickstyle\migrations
  */
-class release_1_4_0 extends migration
+class release_1_4_2 extends migration
 {
 
 	/**
@@ -26,7 +26,7 @@ class release_1_4_0 extends migration
 	 */
 	public function effectively_installed()
 	{
-		return isset($this->config['quickstyle_version']) && version_compare($this->config['quickstyle_version'], '1.4.0', '>=');
+		return isset($this->config['quickstyle_version']) && version_compare($this->config['quickstyle_version'], '1.4.2', '>=');
 	}
 
 	/**
@@ -35,7 +35,7 @@ class release_1_4_0 extends migration
 	static public function depends_on()
 	{
 		return array(
-			'\paybas\quickstyle\migrations\release_1_3_0',
+			'\paybas\quickstyle\migrations\release_1_4_1',
 		);
 	}
 
@@ -45,7 +45,7 @@ class release_1_4_0 extends migration
 	public function update_data()
 	{
 		return array(
-			array('config.update', array('quickstyle_version', '1.4.0')),
+			array('config.update', array('quickstyle_version', '1.4.2')),
 		);
 	}
 }
